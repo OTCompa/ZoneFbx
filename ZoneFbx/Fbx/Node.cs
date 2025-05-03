a6d62dd76c25c1deaddaabf6e5b713947ee24106
OTCompa <41562938+OTCompa@users.noreply.github.com>
2025-05-03 00:05:43 -0500

    Update Node.cs

--- ZoneFbx/Fbx/Node.cs
+++ ZoneFbx/Fbx/Node.cs
@@ -9,6 +9,50 @@ namespace ZoneFbx.Fbx
 {
     internal partial class Node
     {
+        // not really sure how to handle Fbx.Scene.GetRootNode()
+        // i'll figure it out soon:tm: if i really care about doing this
+
+        //internal IntPtr nodePointer { get; private set; }
+        //internal IntPtr managerPointer { get; private set; }
+        //internal string name { get; private set; }
+
+        //public Node(IntPtr manager, string name)
+        //{
+        //    managerPointer = manager;
+        //    this.name = name;
+        //    nodePointer = _Create(manager, name);
+        //}
+
+        //public void SetLclTranslation(double pData0, double pData1, double pData2)
+        //{
+        //    _SetLclTranslation(nodePointer, pData0, pData1, pData2);
+        //}
+
+        //public void AddChild(IntPtr child)  // TODO: Change to Node
+        //{
+        //    _AddChild(nodePointer, child);
+        //}
+
+        //public void AddMaterial(IntPtr material)  // TODO: Change to SurfacePhong
+        //{
+        //    _AddMaterial(nodePointer, material);
+        //}
+
+        //public void SetNodeAttribute(IntPtr mesh)  // TODO: Change to Mesh
+        //{
+        //    _SetNodeAttribute(nodePointer, mesh);
+        //}
+
+        //public void SetStuff(double X, double Y, double Z, int branch)  // TODO: refactor out branch
+        //{
+        //    _SetStuff(nodePointer, X, Y, Z, branch);
+        //}
+
+        //public void SetPostTargetRotation()
+        //{
+        //    _SetPostTargetRotation(nodePointer);
+        //}
+
         [LibraryImport("FBXCSharp.dll", EntryPoint = "FbxNode_Create")]
         [UnmanagedCallConv(CallConvs = [typeof(System.Runtime.CompilerServices.CallConvCdecl)])]
         public static partial IntPtr Create(IntPtr manager, [MarshalAs(UnmanagedType.LPStr)] string name);
